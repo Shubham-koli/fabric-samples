4ee79a89e342a8c4a21110eaf6cbb38ca46f4190
Shubham Koli <shubham.koli@viitor.cloud>
2019-08-08 10:07:29 +0530

    fabcar with minor changes working perfectly for edexa

--- chaincode/fabcar/javascript/lib/fabcar.js
+++ chaincode/fabcar/javascript/lib/fabcar.js
@@ -10,147 +10,68 @@ class FabCar extends Contract {
 
     async initLedger(ctx) {
         console.info('============= START : Initialize Ledger ===========');
-        const cars = [
+        const edexaContract = [
             {
-                color: 'blue',
-                make: 'Toyota',
-                model: 'Prius',
-                owner: 'Tomoko',
+                encryptionKey: 'kxYye694ugubYc7J7P3hu184',
+                hash: '2cd4e52fc1a345da8ad7d6edea15764f9495eed0821e78595d64c9f35a0663d9c863c751794cada6f8e5dfb71c2c1be694f3803c5caa4553efd356de6045fc20',
+                name: '111',
+                note: '111',
+                fileType: 'png'
             },
             {
-                color: 'red',
-                make: 'Ford',
-                model: 'Mustang',
-                owner: 'Brad',
+                encryptionKey: '5OIR4bkyLjmA2baU001maymk',
+                hash: '222959016d0c499e63e95b031042c3cde59d4823737fd6c98adf4ca71b10c09733d0308f62f903bc0cdb5331247f78fa6a17bf315a96baafd8fa92e7c8da99e1022',
+                name: '222',
+                note: '222',
+                fileType: 'png'
             },
             {
-                color: 'green',
-                make: 'Hyundai',
-                model: 'Tucson',
-                owner: 'Jin Soo',
+                encryptionKey: 'fw5tmqhYl08acUBgCxOdo23i',
+                hash: '3d80e377dcf4d1a4dea01ba1a6d32efc952d1449c0a02f7bb079c4a2f2b26f93fcd33b13d71f55992e4a93e50d8ffeafe235482e0afc87a2064b52e32ccd4e05',
+                name: '333',
+                note: '333',
+                fileType: 'png'
             },
             {
-                color: 'yellow',
-                make: 'Volkswagen',
-                model: 'Passat',
-                owner: 'Max',
+                encryptionKey: '1r75S0dksqDcfhyVw9Ak4Mv4',
+                hash: '9cc096a5258ebc51b3795714b2661cdd90b183d728b46fe6e97e461f63bbe299edaaab150a5591bf0571f660cd123aebc77c7985cc0090fd853b8f92f5a300e1',
+                name: '444',
+                note: '444',
+                fileType: 'png'
             },
             {
-                color: 'black',
-                make: 'Tesla',
-                model: 'S',
-                owner: 'Adriana',
-            },
-            {
-                color: 'purple',
-                make: 'Peugeot',
-                model: '205',
-                owner: 'Michel',
-            },
-            {
-                color: 'white',
-                make: 'Chery',
-                model: 'S22L',
-                owner: 'Aarav',
-            },
-            {
-                color: 'violet',
-                make: 'Fiat',
-                model: 'Punto',
-                owner: 'Pari',
-            },
-            {
-                color: 'indigo',
-                make: 'Tata',
-                model: 'Nano',
-                owner: 'Valeria',
-            },
-            {
-                color: 'brown',
-                make: 'Holden',
-                model: 'Barina',
-                owner: 'Shotaro',
+                encryptionKey: '2V7u4oblG4Khx78ez6r68TeP',
+                hash: '7d3f2e8cf925a02bb4be798dffa5f729a213588bd704418847e5ddde13cc8215cc245321477e1ba1018b1bf701cf8ebd75575df344f8e40fc8b30ba5d3b5943a',
+                name: '555',
+                note: '555',
+                fileType: 'png'
             },
         ];
 
-        for (let i = 0; i < cars.length; i++) {
-            cars[i].docType = 'car';
-            await ctx.stub.putState('CAR' + i, Buffer.from(JSON.stringify(cars[i])));
-            console.info('Added <--> ', cars[i]);
+        for (let i = 0; i < edexaContract.length; i++) {
+            edexaContract[i].docType = 'EdexaContract';
+            await ctx.stub.putState('EdexaContract' + i, Buffer.from(JSON.stringify(edexaContract[i])));
+            console.info('Added <--> ', edexaContract[i]);
         }
         console.info('============= END : Initialize Ledger ===========');
     }
 
-    async queryCar(ctx, carNumber) {
-        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
-        if (!carAsBytes || carAsBytes.length === 0) {
-            throw new Error(`${carNumber} does not exist`);
-        }
-        console.log(carAsBytes.toString());
-        return carAsBytes.toString();
-    }
+    async createContract(ctx, uniqueId, encryptionKey, hash, name, note, fileType) {
+        console.info('============= START : Create Contract ===========');
+        console.log(ctx);
 
-    async createCar(ctx, carNumber, make, model, color, owner) {
-        console.info('============= START : Create Car ===========');
-
-        const car = {
-            color,
-            docType: 'car',
-            make,
-            model,
-            owner,
+        const Transaction = {
+            encryptionKey,
+            docType: 'EdexaContract',
+            hash,
+            name,
+            note,
+            fileType
         };
 
-        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
-        console.info('============= END : Create Car ===========');
+        await ctx.stub.putState(uniqueId, Buffer.from(JSON.stringify(Transaction)));
+        console.info('============= END : Create Contract ===========');
     }
-
-    async queryAllCars(ctx) {
-        const startKey = 'CAR0';
-        const endKey = 'CAR999';
-
-        const iterator = await ctx.stub.getStateByRange(startKey, endKey);
-
-        const allResults = [];
-        while (true) {
-            const res = await iterator.next();
-
-            if (res.value && res.value.value.toString()) {
-                console.log(res.value.value.toString('utf8'));
-
-                const Key = res.value.key;
-                let Record;
-                try {
-                    Record = JSON.parse(res.value.value.toString('utf8'));
-                } catch (err) {
-                    console.log(err);
-                    Record = res.value.value.toString('utf8');
-                }
-                allResults.push({ Key, Record });
-            }
-            if (res.done) {
-                console.log('end of data');
-                await iterator.close();
-                console.info(allResults);
-                return JSON.stringify(allResults);
-            }
-        }
-    }
-
-    async changeCarOwner(ctx, carNumber, newOwner) {
-        console.info('============= START : changeCarOwner ===========');
-
-        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
-        if (!carAsBytes || carAsBytes.length === 0) {
-            throw new Error(`${carNumber} does not exist`);
-        }
-        const car = JSON.parse(carAsBytes.toString());
-        car.owner = newOwner;
-
-        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
-        console.info('============= END : changeCarOwner ===========');
-    }
-
 }
 
 module.exports = FabCar;
--- fabcar/javascript/invoke.js
+++ fabcar/javascript/invoke.js
@@ -3,51 +3,187 @@
  */
 
 'use strict';
+// import express from "express";
+const express = require('express');
+const bodyParser =  require('body-parser');
+const cors = require('cors');
+
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const path = require('path');
 
 const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org1.json');
 
-async function main() {
-    try {
 
-        // Create a new file system based wallet for managing identities.
-        const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = new FileSystemWallet(walletPath);
-        console.log(`Wallet path: ${walletPath}`);
 
-        // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists('user1');
-        if (!userExists) {
-            console.log('An identity for the user "user1" does not exist in the wallet');
-            console.log('Run the registerUser.js application before retrying');
-            return;
+let gettxid = (asset) => {
+    return new Promise((resolve, reject) => {
+        async function getid(asset) {
+            try {
+
+                // Create a new file system based wallet for managing identities.
+                const walletPath = path.join(process.cwd(), 'wallet');
+                const wallet = new FileSystemWallet(walletPath);
+                console.log(`Wallet path: ${walletPath}`);
+
+                // Check to see if we've already enrolled the user.
+                const userExists = await wallet.exists('user1');
+                if (!userExists) {
+                    console.log('An identity for the user "user1" does not exist in the wallet');
+                    console.log('Run the registerUser.js application before retrying');
+                    return;
+                }
+
+                // Create a new gateway for connecting to our peer node.
+                const gateway = new Gateway();
+                await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true }} );
+
+                // Get the network (channel) our contract is deployed to.
+                const network = await gateway.getNetwork('mychannel');
+
+                // Get the contract from the network.
+                const contract = network.getContract('fabcar');
+
+                // Submit the specified transaction.
+                // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+                // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
+                //console.log(asset.carNo);
+                // let test = await contract.submitTransaction('createCar',asset.someUniqueID , JSON.stringify(asset.encryptionKey), JSON.stringify(asset.hash), JSON.stringify(asset.name), JSON.stringify(asset.note),JSON.stringify(asset.fileType));
+                const myTx = contract.createTransaction('createContract');
+                if(!asset.someUniqueID){
+                    throw error;
+                }
+                if(!asset.encryptionKey){
+                    throw error;
+                }
+                if(!asset.hash){
+                    throw error;
+                }
+                if(!asset.name){
+                    asset.name = 'NA';
+                }
+                if(!asset.note){
+                    asset.note = 'NA';
+                }
+                if(!asset.fileType){
+                    throw error;
+                }
+                console.log('test');
+                await myTx.submit(asset.someUniqueID, JSON.stringify(asset.encryptionKey), JSON.stringify(asset.hash), JSON.stringify(asset.name), JSON.stringify(asset.note),JSON.stringify(asset.fileType));
+                const txId = myTx.getTransactionID();
+                console.log('Transaction has been submitted');
+                // console.log(txId);
+
+                // Disconnect from the gateway.
+                await gateway.disconnect();
+                console.log('executed');
+                resolve(txId);
+            } catch (error) {
+                console.error(`Failed to submit transaction: ${error}`);
+                reject(error);
+                process.exit(1);
+            }
         }
+        getid(asset);
+    });
 
-        // Create a new gateway for connecting to our peer node.
-        const gateway = new Gateway();
-        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+};
 
-        // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
 
-        // Get the contract from the network.
-        const contract = network.getContract('fabcar');
 
-        // Submit the specified transaction.
-        // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
-        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom');
-        console.log('Transaction has been submitted');
+let getdetails = (txid) => {
+    return new Promise((resolve, reject) => {
+        async function tmp (txid) {
+            try{
+                if(!txid)
+                {
+                    throw error;
+                }
+                // Create a new file system based wallet for managing identities.
+                const walletPath = path.join(process.cwd(), 'wallet');
+                const wallet = new FileSystemWallet(walletPath);
+                console.log(`Wallet path: ${walletPath}`);
 
-        // Disconnect from the gateway.
-        await gateway.disconnect();
+                // Check to see if we've already enrolled the user.
+                const userExists = await wallet.exists('user1');
+                if (!userExists) {
+                    console.log('An identity for the user "user1" does not exist in the wallet');
+                    console.log('Run the registerUser.js application before retrying');
+                    return;
+                }
 
-    } catch (error) {
-        console.error(`Failed to submit transaction: ${error}`);
-        process.exit(1);
+                // Create a new gateway for connecting to our peer node.
+                const gateway = new Gateway();
+                await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true }} );
+
+                // Get the network (channel) our contract is deployed to.
+                const network = await gateway.getNetwork('mychannel');
+                const channel = network.getChannel();
+                // Get the contract from the network.
+                // const contract = network.getContract('fabcar');
+
+                let response_payload = await channel.queryTransaction(txid);
+                resolve (response_payload);
+            }catch(err){
+                console.log(err);
+                reject(err);
+            }
+        }
+        tmp(txid);
+    });
+};
+
+
+app.post('/add-transaction',(req, response) => {
+    console.log(req.body);
+    req.body.someUniqueID = req.body.edexaContractNo;
+    delete req.body.edexaContractNo;
+    gettxid(req.body).then(res => {
+        delete res._nonce;
+        delete res._admin;
+        res.transactionId = res._transaction_id;
+        delete res._transaction_id;
+        response.send(res);
+    }).catch (err => {
+        response.sendStatus(500);
+        console.log(err);
+    });
+    // response.send(txid);
+    // test(req.body);
+    // response.sendStatus(200);
+});
+
+app.post('/transaction',(req, response) => {
+    console.log(req.body);
+    try{
+        // let details = getDetaills(req.body.txid);
+        getdetails(req.body.id).then(res => {
+            let output = res.transactionEnvelope.payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[0].rwset.writes;
+            delete output.is_delete;
+            let withoutSlashes = output[0].value.replace(/\\/g, "");
+            let withoutQuotes = withoutSlashes.replace(/""/g, '"');
+            let final = JSON.parse(withoutQuotes);
+            final.transactionId = req.body.id;
+            let d = new Date();
+            let n = d.toISOString();
+            final.timestamp = n;
+            // console.log({final});
+            response.send(final);
+        });
+    }catch (err){
+        response.sendStatus(500);
+        console.log('err');
     }
-}
+});
 
-main();
+//transactionEnvelope.payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[1].rwset.writes
+
+
+app.listen(4000, () => {
+    console.log('Started on port 4000');
+});
